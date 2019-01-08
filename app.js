1299433619c5a6d99fcc03d6ff3f68153801b0e3
Bishal Deb <bishaldeb656@gmail.com>
2019-01-08 23:35:31 +0530

    Database added.

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
 var express         = require("express"),
-    bodyParser      = require("body-parser");
+    mongoose        = require("mongoose"),
+    bodyParser      = require("body-parser"),
     app             = express();
     
 //APP INITIALIZE
+mongoose.connect("mongodb://localhost:27017/scenicguides", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
-var campgrounds = [
-]
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create
+
+
 // HOME PAGE
 app.get("/", function(req, res){
     res.render("landing");
 })
 
-app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-});
-
-
-// ALL SITES PAGE
+// INDEX - SHOW ALL SITES
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCapmgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCapmgrounds});
+        }
+    });
 });
 
-//NEW SITE
+//NEW - ADD SITE
 app.get("/campgrounds/new",function(req, res){
     res.render("new");
 });
 
+//CREATE - CREATE SITE
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 
 app.listen(1500, function(){
