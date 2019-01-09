2c879d375d7558961e0852e5fe59ab5a08e5dce1
Bishal Deb <bishaldeb656@gmail.com>
2019-01-09 21:37:56 +0530

    Posts linked with users.

--- models/campground.js
+++ models/campground.js
@@ -4,10 +4,17 @@ var campgroundSchema = new mongoose.Schema({
     name        : String,
     image       : String,
     description : String,
+    author      : {
+        id      : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        },
+        username: String
+    },
     comments    : [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
+            ref : "Comment"
         }
     ]
 });
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,16 +14,20 @@ router.get("/", function(req, res){
 });
 
 //NEW - ADD SITE
-router.get("/new",function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
 //CREATE - CREATE SITE
-router.post("/", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+router.post("/", isLoggedIn, function(req, res){
+    var name    = req.body.name;
+    var image   = req.body.image;
+    var desc    = req.body.description;
+    var author  = {
+        id      : req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
