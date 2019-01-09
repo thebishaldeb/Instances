c3ec70c77e70e1141da15c76caead25fe99b217b
Bishal Deb <bishaldeb656@gmail.com>
2019-01-09 20:58:31 +0530

    Username saved with comments.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.connect("mongodb://localhost:27017/scenicguides", { useNewUrlParser: tr
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
-seedDB();
+// seedDB();
 
 // PASSPORT CONFIGURATION 
 app.use(require("express-session")({
--- routes/comments.js
+++ routes/comments.js
@@ -25,6 +25,9 @@ router.post("/",isLoggedIn, function(req, res){
                 if(err){
                     console.log(err);
                 } else {
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
                     found.comments.push(comment);
                     found.save();
                     res.redirect("/campgrounds/" + found._id);
