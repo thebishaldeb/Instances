e61dafa7c4d5ea54e3bb30648bd9ba23037b3500
devyaniChoubey <devyanichoubey16@gmail.com>
2019-12-06 22:08:09 +0530

    Edit Blog Page Redesign #22

--- models/usermodel.js
+++ models/usermodel.js
@@ -3,7 +3,9 @@ var mongoose    = require("mongoose"),
 
 var UserSchema  = new mongoose.Schema({
     username    : String,
-    password    : String
+    password    : String,
+    
+
 });
 
 UserSchema.plugin(passportLocalMongoose);
