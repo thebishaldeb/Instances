16e8af6f478d72056cdc2413bfa939d3502f37e1
devyaniChoubey <devyanichoubey16@gmail.com>
2019-12-07 10:46:52 +0530

    Increase Schema For User

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,15 +1,33 @@
-var mongoose    = require("mongoose"),
-    passportLocalMongoose   = require("passport-local-mongoose");
+var mongoose = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
-var UserSchema  = new mongoose.Schema({
-    username    : String,
-    password    : String,
-    email       :String,
-    phonenumber :String,
-    profilepicture : String 
-
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    phonenumber: String,
+    profilepicture: String,
+    name: String,
+    age: String,
+    gender: String,
+    description: String,
+    birthdate: {
+        type: Date,
+        default: Date.now
+    },
+    role: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports  = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
